dc710e51d3aaedeae3fe7761a4c4d2ad203dbd29
trynantapat009 <110448881+trynantapat009@users.noreply.github.com>
2023-03-11 13:27:32 +0700

    Add files via upload

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,57 @@
+var quizQuestions = [
+    {
+        question: "aaaaaa",
+        answers: [
+            {
+                type: "aaa",
+                content: "aaa"
+            },
+            {
+                type: "aaa",
+                content: "aaa"
+            },
+            {
+                type: "aaaa",
+                content: "aaa"
+            }
+        ]
+    },
+    {
+        question: "bbbb",
+        answers: [
+            {
+                type: "bbb",
+                content: "bbb"
+            },
+            {
+                type: "bbb",
+                content: "bbb"
+            },
+            {
+                type: "bbb",
+                content: "bbb"
+            }
+        ]
+    },
+    {
+        question: "ccc",
+        answers: [
+            {
+                type: "ccc",
+                content: "ccc"
+            },
+            {
+                type: "ccc",
+                content: "ccc"
+            },
+            {
+                type: "ccc",
+                content: "ccc"
+            }
+        ]
+    },
+    
+  ];
+  
+  export default quizQuestions;
+  
